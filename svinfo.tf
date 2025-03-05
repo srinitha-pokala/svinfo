987781b8277c141cbb817e2e4adef6691c8e654c
srinitha-pokala <srinithapokala@gmail.com>
2025-03-05 15:06:54 +0530

    Create svinfo.tf

---
+++ svinfo.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "ap-south-2"  # Updated AWS region
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "ss_s3" {
+  bucket = "svinfoccitbhcket5"
+  tags = { Name = "ss-public-bucket" }
+}
